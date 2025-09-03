7f2322cc5915ac23c20ac70e5555fe6c4aab5a95
Abhishek Tiwari <163150228+ABHISHEK1618TIWARI@users.noreply.github.com>
2025-09-03 22:05:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ app.post("/api/schools", upload.single("image"), async (req, res) => {
       return res.status(400).json({ error: "Image is required." });
     }
 
-    const imageFilename = req.file.path;
+    const imageFilename = req.file.fileName;
 
     const sql = `
       INSERT INTO schools (name, address, city, state, contact, image, email_id)
